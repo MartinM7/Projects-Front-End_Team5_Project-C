33e0ff15bcffe0a3f2cbd3cdde25685915fe3a66
Gergely Izsak <izsak.gera@gmail.com>
2021-07-19 20:55:08 +0200

    typos and routing for portfolio

--- ProjectsFrontEnd/src/app/app-routing.module.ts
+++ ProjectsFrontEnd/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 const routes: Routes = [
@@ -17,6 +18,9 @@ const routes: Routes = [
   },
   {
     path: "testimonials", component: TestimonialsComponent
+  },
+  {
+    path: "portfolio", component: PortfolioComponent
   }
 ];
 
