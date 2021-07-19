a3704974d51e44b4c29668c12d2f9da1f529c3b4
Gergely Izsak <izsak.gera@gmail.com>
2021-07-19 11:06:40 +0200

    all the basic stuff

--- ProjectsFrontEnd/src/app/app.module.ts
+++ ProjectsFrontEnd/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
